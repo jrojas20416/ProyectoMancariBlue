6960838b38f94bd817d506a634ac08ff53e871de
jrojas20416 <jrojas20416@ufide.ac.cr>
2024-10-14 20:41:48 -0600

    Cambios

---
+++ ProyectoMancariBlue/Models/Obj/Request/ReporteRequest.cs
@@ -0,0 +1,13 @@
+﻿using ProyectoMancariBlue.Models.Obj.DTO;
+
+namespace ProyectoMancariBlue.Models.Obj.Request
+{
+    public class ReporteRequest
+    {
+        public IEnumerable<ReporteDTO> listaReporte { get; set; }
+        public IEnumerable<VentaDTO> listaTransacciones { get; set; }
+        public ReporteDTO ReporteCreate { get; set; }
+        public ReporteDTO ReporteModify { get; set; }
+
+    }
+}
---
+++ ProyectoMancariBlue/Models/Obj/Request/TransaccionRequest.cs
@@ -0,0 +1,16 @@
+﻿using ProyectoMancariBlue.Models.Obj.DTO;
+
+namespace ProyectoMancariBlue.Models.Obj.Request
+{
+    public class TransaccionRequest
+    {
+        public List<CompraDTO> CompraDTOs { get; set;}
+
+        public List<VentaDTO> ventaDTOs { get; set; }
+        public List<ProveedorDTO> proveedorDTOs { get; set; }
+        public List<AnimalDTO>VentaListaAnimal { get; set; }
+
+        public CompraDTO CompraCreate { get; set; }
+        public VentaDTO VentaCreate { get; set; }
+    }
+}
